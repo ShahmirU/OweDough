49b416ba31ffe0dbed7f81c4e6caf8f8f8d270f8
Nancy Zhao <nancy.zhao@outlook.com>
2019-09-14 18:05:46 -0400

    something

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var app = express();
 
-<<<<<<< HEAD
-// var firebase = require("firebase-admin");
-// var serviceAccount = require("./serviceAccountKey.json");
-
-//firebase.initializeApp({
-  //      credential: firebase.credential.cert(serviceAccount),
-    //    databaseURL: https://owedough-531a7.firebaseio.com/
-// });
-=======
 //var firebase = require("firebase-admin");
 //var serviceAccount = require("./serviceAccountKey.json");
 
@@ -17,24 +8,12 @@ var app = express();
 //        credential: firebase.credential.cert(serviceAccount),
 //        databaseURL: https://owedough-531a7.firebaseio.com/
 //});
->>>>>>> f3f4dbc71f80b096d71d7b0667ee50d0df135687
 
 
 // Set the configuration for your app
   // TODO: Replace with your project's config object
 //  var config = {
   //  apiKey: "AIzaSyDhlB_9CPCoQJprIgdxdqpFoPdC44ZEN3I",
-<<<<<<< HEAD
-   // authDomain: "owedough-531a7.firebaseapp.com",
-   // databaseURL: "https://owedough-531a7.firebaseio.com",
-   // storageBucket: "owedough-531a7.appspot.com"
- // };
-
-//  firebase.initializeApp(config);
-
-  // Get a reference to the database service
- // var database = firebase.database();
-=======
     //authDomain: "owedough-531a7.firebaseapp.com",
     //databaseURL: "https://owedough-531a7.firebaseio.com",
     //storageBucket: "owedough-531a7.appspot.com"
@@ -44,7 +23,6 @@ var app = express();
 
   // Get a reference to the database service
   //var database = firebase.database();
->>>>>>> f3f4dbc71f80b096d71d7b0667ee50d0df135687
 
 // routes
 
