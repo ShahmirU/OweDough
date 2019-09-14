474c39da2830ed14dfc0390a0033cd3594b3f316
Kashaf Salaheen <kashafsalaheen@v1044-wn-rt-b-134-73.campus-dynamic.uwaterloo.ca>
2019-09-14 14:21:28 -0400

    server.js connection

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+var firebase = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+firebase.initializeApp({
+        credential: firebase.credential.cert(serviceAccount),
+        databaseURL: https://owedough-531a7.firebaseio.com/
+});
+
+
 // routes
 
 app.use(express.static(__dirname + '/public'));
@@ -13,3 +22,4 @@ app.get('/dashboard', function(request, response){
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
