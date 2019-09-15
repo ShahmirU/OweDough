1c1a78a73cbcbb2e7d7540f6720a21c80367c0f1
usmansha <shahmir.usman@mail.utoronto.ca>
2019-09-15 04:22:48 -0400

    pushing server

--- server.js
+++ server.js
@@ -23,25 +23,27 @@ app.get('/breakdown', function (request, response) {
   console.log('GET request received at /breakdown');
 });
 
+
 app.get('/submit', function(req, response){
-    var first_name = req.query.first_name;
-    var last_name = req.query.last_name;
-    var amount = Math.abs(Number(req.query.amount));
-    var date = req.query.date;
-    var desc = req.query.desc;
-    console.log(first_name);
-    console.log(last_name);
+  var first_name = req.query.first_name;
+  var last_name = req.query.last_name;
+  var amount = req.query.amount;
+  var date = req.query.date;
+  var desc = req.query.desc;
+  console.log(amount);
+  console.log(first_name);
+  console.log(last_name);
 
-    var payments = firebase.database().ref("payments");
+  var payments = firebase.database().ref("payments");
 
-    payments.push( {
-      "first_name": first_name,
-      "last_name": last_name,
-      "amount": amount,
-      "date": date,
-      "desc": desc,
-    });
-    
+  payments.push( {
+    "first_name": first_name,
+    "last_name": last_name,
+    "amount": amount,
+    "date": date,
+    "desc": desc,
+  });
+  
 });
 
 app.listen(3000, function () {
