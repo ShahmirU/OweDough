bf01acd27ba2bdbb882d1d449f2a9a1c2bfa1eeb
usmansha <shahmir.usman@mail.utoronto.ca>
2019-09-14 21:40:47 -0400

    pushed form logic

---
+++ public/new.js
@@ -0,0 +1,21 @@
+
+
+$('#submit_btn').on("click", function(){
+    submit_new($("#first_name"), $("#last_name"), $("#amount"), $("#date"), $("#desc"));
+});
+
+
+function submit_new(first_name, last_name, amount, date, desc){
+	$(document).ready(function () {
+        $.get('/predict'+'?first_name='+first_name+'&last_name='+last_name+'&amount='+amount+'&date='+date+'&desc='+desc, function(data){
+            if(!data){
+                console.log("Unable to submit");
+                alert("Error: Unable to submit");
+            }
+            else{
+                console.log(data);
+                //TODO
+            }
+        })
+    });  
+}
